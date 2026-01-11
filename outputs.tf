fec23beea5fc6f437d9221982cd95eaeda5dee40
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:42:43 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "logic_app_integration_account_maps" {
+  description = "All logic_app_integration_account_map resources"
+  value       = azurerm_logic_app_integration_account_map.logic_app_integration_account_maps
+}
+
