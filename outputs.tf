b2a594dc2d93dc5cb67ce77478ab01bd88d505e4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "logic_app_integration_account_maps" {
-  description = "All logic_app_integration_account_map resources"
-  value       = azurerm_logic_app_integration_account_map.logic_app_integration_account_maps
-}
 output "logic_app_integration_account_maps_content" {
-  description = "List of content values across all logic_app_integration_account_maps"
-  value       = [for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : v.content]
+  description = "Map of content values across all logic_app_integration_account_maps, keyed the same as var.logic_app_integration_account_maps"
+  value       = { for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : k => v.content }
 }
 output "logic_app_integration_account_maps_integration_account_name" {
-  description = "List of integration_account_name values across all logic_app_integration_account_maps"
-  value       = [for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : v.integration_account_name]
+  description = "Map of integration_account_name values across all logic_app_integration_account_maps, keyed the same as var.logic_app_integration_account_maps"
+  value       = { for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : k => v.integration_account_name }
 }
 output "logic_app_integration_account_maps_map_type" {
-  description = "List of map_type values across all logic_app_integration_account_maps"
-  value       = [for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : v.map_type]
+  description = "Map of map_type values across all logic_app_integration_account_maps, keyed the same as var.logic_app_integration_account_maps"
+  value       = { for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : k => v.map_type }
 }
 output "logic_app_integration_account_maps_metadata" {
-  description = "List of metadata values across all logic_app_integration_account_maps"
-  value       = [for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : v.metadata]
+  description = "Map of metadata values across all logic_app_integration_account_maps, keyed the same as var.logic_app_integration_account_maps"
+  value       = { for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : k => v.metadata }
 }
 output "logic_app_integration_account_maps_name" {
-  description = "List of name values across all logic_app_integration_account_maps"
-  value       = [for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : v.name]
+  description = "Map of name values across all logic_app_integration_account_maps, keyed the same as var.logic_app_integration_account_maps"
+  value       = { for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : k => v.name }
 }
 output "logic_app_integration_account_maps_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_integration_account_maps"
-  value       = [for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_integration_account_maps, keyed the same as var.logic_app_integration_account_maps"
+  value       = { for k, v in azurerm_logic_app_integration_account_map.logic_app_integration_account_maps : k => v.resource_group_name }
 }
 
